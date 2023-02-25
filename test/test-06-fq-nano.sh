91fd98da199a14a2888ea6db923cd3a98d525452
Marco van Zwetselaar <io@zwets.it>
2023-02-25 23:15:33 +0300

    Make test-06-fq-nano not fail on tiny diff

--- test/test-06-fq-nano.sh
+++ test/test-06-fq-nano.sh
@@ -12,7 +12,7 @@ export BAP_DB_DIR="$BASE_DIR/databases"
 make_output_dir
 run_bap -v -o "$OUTPUT_DIR" "$BASE_DIR/data/nano.fq.gz"
 check_output || {
-  echo "CHECK the above, probably good"
+  echo "CHECK the above, probably good, just tiny differences in counts"
   true
 }
 
